688f7b7dada6dbc50c696ed1046a80eb43f4d9ca
Anuj <anujdeswal111@gmail.com>
2026-09-02 22:39:24 +0530

    adding Decoder architecture

--- model.py
+++ model.py
@@ -166,4 +166,35 @@ class Encoder(nn.Module):
         return self.norm(x)
         
         
+# DECODER BLOCK
+class DecoaderBlock(nn.Module):
+    
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block:MultiHeadAttentionBlock, feed_forward_block:FeedForwardBlock, dropout: float):
+        super.__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connection = nn.Module([ResidualConnection(dropout) for _ in range(3)])
+        
+    def forward(self, x, encoder_output, src_mask, target_mask):
+        x = self.residual_connection[0](x, lambda x: self.self_attention_block(x,x,x, target_mask))
+        x = self.residual_connection[0](x, lambda x: self.cross_attention_block(x,encoder_output,encoder_output, src_mask))
+        x = self.residual_connection[0](x,self.feed_forward_block)
+        return x
+    
+# DECODER
+class Decoder(nn.Module):
+    
+    def __init__(self, layers: nn.ModuleList):
+        super.__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+        
+    def forward(self,x,encoader_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoader_output, src_mask, tgt_mask)
+            
+        return self.norm(x)
+    
+
         
