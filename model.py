16cfddd46a3c4d5d78eee5878be98df253fe3bfd
Anuj <anujdeswal111@gmail.com>
2026-09-02 22:44:32 +0530

    adding last linear ayer architecture

--- model.py
+++ model.py
@@ -196,5 +196,16 @@ class Decoder(nn.Module):
             
         return self.norm(x)
     
+    
+# PROJECTION/ LINEAR LAYER
+class ProjectionLayer(nn.Module):
+    
+    def __init__(self, d_model:int, vocab_size:int):
+        super.__init__()
+        self.proj = nn.Linear(d_model,vocab_size)
+        
+    def forward(self,x):
+        return torch.log_softmax(self.proj(x), dim = -1)
+
 
         
